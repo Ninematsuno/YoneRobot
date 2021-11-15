50735a0e71c818355e7da95370792e45cebeedb1
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-11-15 18:21:15 +0530

    Update games.py

--- YoneRobot/modules/games.py
+++ YoneRobot/modules/games.py
@@ -64,13 +64,3 @@ async def _(event):
 
 
 
-__help__ = """
- *Play Game With Emojis:*
-  - /dice or /dice 1 to 6 any value
-  - /ball or /ball 1 to 5 any value
-  - /dart or /dart 1 to 6 any value
- Usage: hahaha just a magic.
- warning: you would be in trouble if you input any other value than mentioned.
-"""
-
-__mod_name__ = "Game"
